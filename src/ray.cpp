e4abc7a30693413836e493c6986cd92beead6d8a
jplmoreira <jplmoreira92@gmail.com>
2019-03-10 16:56:06 +0000

    Add ray class to project

---
+++ src/ray.cpp
@@ -0,0 +1,26 @@
+#include "ray.hpp"
+
+p3d::ray::ray() {}
+
+p3d::ray::ray(math::vec3 &o, math::vec3 &d) {
+	_o = o;
+	_d = d;
+}
+
+p3d::ray::~ray() {}
+
+math::vec3 p3d::ray::o() const {
+	return _o;
+}
+
+math::vec3 p3d::ray::d() const {
+	return _d;
+}
+
+void p3d::ray::o(const math::vec3 &o) {
+	_o = o;
+}
+
+void p3d::ray::d(const math::vec3 &d) {
+	_d = d;
+}
---
+++ src/ray.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "math/vec3.hpp"
+
+namespace p3d {
+	class ray {
+		math::vec3 _o;
+		math::vec3 _d;
+	public:
+		ray();
+		ray(math::vec3 &, math::vec3 &);
+		~ray();
+
+		math::vec3 o() const;
+		math::vec3 d() const;
+
+		void o(const math::vec3 &);
+		void d(const math::vec3 &);
+	};
+}
