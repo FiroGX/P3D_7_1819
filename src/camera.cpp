9bcbe9d15f617ae76f89b88835fa438ae0a59262
Marta Mendes <martamendes@machiavelli.local>
2019-03-12 18:24:16 +0000

    Add comments to primary ray implementation

--- src/camera.cpp
+++ src/camera.cpp
@@ -2,27 +2,27 @@
 
 p3d::camera::camera(math::vec3 *eye, math::vec3 *at, math::vec3 *up, float hither, float fov, int resX, int resY) {
 
-	_eye = eye; _at = at;	_up = up;	_hither = hither;
+	_eye = eye; _at = at;	_up = up;	_hither = hither; //attributions
 	_fov = fov; _resX = resX; _resY = resY;
 
-	_ze = math::normalize(_eye - _at);
-	_xe = math::normalize(math::cross(_up, _ze));
-	_ye = math::cross(_ze, _xe);
+	_ze = math::normalize(_eye - _at);						//vector from at to the eye
+	_xe = math::normalize(math::cross(_up, _ze)); //cross between up vector and Ze
+	_ye = math::cross(_ze, _xe);									//cross between Ze and Xe
 
-	_height = 2 * _ze * tanf(_fovy / 2);
-	_width = (resX() / resY()) * _height;
+	_height = 2 * _ze * tanf(_fovy / 2);					//height and width calculations
+	_width = (resX() / resY()) * _height;					//based on resolution and fovy
 }
 
 p3d::camera::~camera() {}
 
 ray camera::primaryRay(int x, int y) {
-	ray ray;
-	math::vec3 df = math::normalize(_at - _eye);
-	float pixel_x = _width * (x / _resX - 0.5);
-	float pixel_y = _height * (y / _resY - 0.5);
+	math::vec3 df = math::normalize(_at - _eye); 	//df vector from eye to at
+	float pixel_x = _width * (x / _resX - 0.5);		//x component on the pixel
+	float pixel_y = _height * (y / _resY - 0.5);	//y component on the pixel
 
-	ray.o(*_eye);
-	ray.d(math::normalize(-df * _ze + pixel_x * _xe + pixel_y * _ye));
+	ray ray;
+	ray.o(*_eye);			//origin of the ray is the eye
+	ray.d(math::normalize(-df * _ze + pixel_x * _xe + pixel_y * _ye)); //formula
 	return ray;
 }
 
