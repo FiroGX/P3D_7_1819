6c93c5ca95bced0aedc71e5494c6d5c21dd4009e
martadmendes <marta.durand@gmail.com>
2019-03-12 23:02:29 +0000

    Fix implementation errors on camera and primary rays

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,52 +1,53 @@
+#include <cmath>
 #include "camera.hpp"
 
-p3d::camera::camera(math::vec3 *eye, math::vec3 *at, math::vec3 *up, float hither, float fov, int resX, int resY) {
+p3d::camera::camera(math::vec3 eye, math::vec3 at, math::vec3 up, float hither, float fov, int resX, int resY) {
 
 	_eye = eye; _at = at;	_up = up;	_hither = hither; //attributions
 	_fov = fov; _resX = resX; _resY = resY;
 
-	_ze = math::normalize(_eye - _at);						//vector from at to the eye
-	_xe = math::normalize(math::cross(_up, _ze)); //cross between up vector and Ze
-	_ye = math::cross(_ze, _xe);									//cross between Ze and Xe
+	_ze = math::normalize(_eye - _at);				  //vector from at to the eye
+	_xe = math::normalize(math::cross(_up, _ze));	  //cross between up vector and Ze
+	_ye = math::cross(_ze, _xe);					  //cross between Ze and Xe
 
-	_height = 2 * _ze * tanf(_fovy / 2);					//height and width calculations
-	_width = (resX() / resY()) * _height;					//based on resolution and fovy
+	_df = (_at - _eye).magnitude(); 				 //distance from eye to at
+	_height = 2.0f * _df * std::tanf(_fov / 2);		 //height and width calculations
+	_width = (_resX / _resY) * _height;				 //based on resolution and fovy
 }
 
 p3d::camera::~camera() {}
 
-ray camera::primaryRay(int x, int y) {
-	math::vec3 df = math::normalize(_at - _eye); 	//df vector from eye to at
+p3d::ray p3d::camera::primaryRay(int x, int y) {
 	float pixel_x = _width * (x / _resX - 0.5);		//x component on the pixel
 	float pixel_y = _height * (y / _resY - 0.5);	//y component on the pixel
 
 	ray ray;
-	ray.o(*_eye);			//origin of the ray is the eye
-	ray.d(math::normalize(-df * _ze + pixel_x * _xe + pixel_y * _ye)); //formula
+	ray.o(_eye);									//origin of the ray is the eye
+	ray.d(math::normalize(-_df * _ze + pixel_x * _xe + pixel_y * _ye)); //formula
 	return ray;
 }
 
-math::vec3 *p3d::camera::eye() const {
+math::vec3 p3d::camera::eye() const {
 	return _eye;
 }
 
-math::vec3 *p3d::camera::at() const {
+math::vec3 p3d::camera::at() const {
 	return _at;
 }
 
-math::vec3 *p3d::camera::up() const {
+math::vec3 p3d::camera::up() const {
 	return _up;
 }
 
-math::vec3 *p3d::camera::xe() const {
+math::vec3 p3d::camera::xe() const {
 	return _xe;
 }
 
-math::vec3 *p3d::camera::ye() const {
+math::vec3 p3d::camera::ye() const {
 	return _ye;
 }
 
-math::vec3 *p3d::camera::ze() const {
+math::vec3 p3d::camera::ze() const {
 	return _ze;
 }
 
@@ -64,6 +65,10 @@ float p3d::camera::width() const {
 
 float p3d::camera::height() const {
 	return _height;
+}
+
+float p3d::camera::df() const {
+	return _df;
 }
 
 int p3d::camera::resX() const {
--- src/camera.hpp
+++ src/camera.hpp
@@ -1,36 +1,40 @@
 #pragma once
 #include "math/vec3.hpp"
+#include "ray.hpp"
 
 namespace p3d {
 	class camera {
-		math::vec3 *_eye;
-		math::vec3 *_at;
-		math::vec3 *_up;
-		math::vec3 *_xe;
-		math::vec3 *_ye;
-		math::vec3 *_ze;
-		float 		_hither;
-		float 		_fov;
-		float 		_width;
-		float 		_height;
-		int 			_resX;
-		int 			_resY;
+		math::vec3 _eye;
+		math::vec3 _at;
+		math::vec3 _up;
+		math::vec3 _xe;
+		math::vec3 _ye;
+		math::vec3 _ze;
+		float	   _hither;
+		float 	   _fov;
+		float 	   _width;
+		float 	   _height;
+		float	   _df;
+		int		   _resX;
+		int		   _resY;
 
 	public:
-		camera(math::vec3*, math::vec3*, math::vec3*, float, float, int, int);
+		camera(math::vec3, math::vec3, math::vec3, float, float, int, int);
 
 		~camera();
 
-		math::vec3 *eye() const;
-		math::vec3 *at() const;
-		math::vec3 *up() const;
-		math::vec3 *xe() const;
-		math::vec3 *ye() const;
-		math::vec3 *ze() const;
+		ray primaryRay(int, int);
+		math::vec3 eye() const;
+		math::vec3 at() const;
+		math::vec3 up() const;
+		math::vec3 xe() const;
+		math::vec3 ye() const;
+		math::vec3 ze() const;
 		float hither() const;
 		float fov() const;
 		float width() const;
 		float height() const;
+		float df() const;
 		int resX() const;
 		int resY() const;
 	};
